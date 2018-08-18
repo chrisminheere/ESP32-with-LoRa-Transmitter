878bc982424ff719bd097446e8f277120fd6c1cd
Chris minheere <minheerechris@gmail.com>
2018-08-18 13:56:32 +0200

    Add files via upload

--- LoRaSender868MHz_DS18B20/LoRaSender868MHz_DS18B20.ino
+++ LoRaSender868MHz_DS18B20/LoRaSender868MHz_DS18B20.ino
@@ -1,22 +1,22 @@
 //*****************************************************************************************************************************//
-//Made By: Chris Minheere 
-//16-08-2018                                                                                                                              
-//Lora Transmitter 868Mhz, On ESP32. 
-//Hardware: ESP32, DS18b20, SX1276 868MHz                                                                                                     
-                                                                                                                                    
-                                                                                                                                     
-//Sketch uses 117026 bytes (8%) of program storage space. Maximum is 1310720 bytes.                                                 
-//Global variables use 9828 bytes (3%) of dynamic memory, leaving 285084 bytes for local variables. Maximum is 294912 bytes.        
+//Made By: Chris Minheere
+//16-08-2018
+//Lora Transmitter 868Mhz, On ESP32.
+//Hardware: ESP32, DS18b20, SX1276 868MHz
+
+
+//Sketch uses 117026 bytes (8%) of program storage space. Maximum is 1310720 bytes.
+//Global variables use 9828 bytes (3%) of dynamic memory, leaving 285084 bytes for local variables. Maximum is 294912 bytes.
 //*****************************************************************************************************************************//
 
 
 #include <SPI.h>
 #include <LoRa.h>
-#include <OneWire.h> 
+#include <OneWire.h>
 #include <DallasTemperature.h>
 
 int counter = 0;
-#define ONE_WIRE_BUS 32 
+#define ONE_WIRE_BUS 32
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 
@@ -32,16 +32,19 @@ void setup() {
     Serial.println("Starting LoRa failed!");
     while (1);
   }
-  //pinMode(ONE_WIRE_BUS,INPUT_PULLUP);
+
+  //**********Lora Transceiver Settings***********//
+  //More ino at:https://github.com/sandeepmistry/arduino-LoRa/blob/master/API.md
+
   sensors.begin();
-  LoRa.setTxPower(20);
+  LoRa.setTxPower(20);                    //Change the TX power of the radio.
   //LoRa.setTxPower(txPower, outputPin);
-  LoRa.setSpreadingFactor(12);
-  LoRa.setSignalBandwidth(125E3);
-  LoRa.setCodingRate4(4/5);
-  LoRa.setSyncWord(0x13);
-  //LoRa.disableCrc();
-  LoRa.setPreambleLength(8);
+  LoRa.setSpreadingFactor(12);            //Change the spreading factor of the radio.
+  LoRa.setSignalBandwidth(125E3);         //Change the signal bandwidth of the radio.
+  LoRa.setCodingRate4(4 / 5);             //Change the coding rate of the radio.
+  LoRa.setSyncWord(0x13);                 //Change the sync word of the radio.
+  //LoRa.disableCrc();                    //Enable or disable CRC usage, by default a CRC is not used.
+  LoRa.setPreambleLength(8);              //Change the preamble length of the radio.
 }
 
 void loop() {
